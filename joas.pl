ee423d6479f115a5893e0c383ad0821c13e8594c
balaiagem <62041067+balaiagem@users.noreply.github.com>
2020-11-29 15:35:00 -0300

    Add files via upload

---
+++ joas.pl
@@ -0,0 +1,18 @@
+quantia_poupada(21000).
+gantos(25000, estavel).
+dependentes(3).
+
+poupanca_min(K, Y):- K is Y * 5000.
+conta_poupanca(adequada):- quantia_poupada(X), dependentes(Y), poupanca_min(K, Y), X > K.
+conta_poupanca(inadequada):- quantia_poupada(X), dependentes(Y), poupanca_min(K, Y), X < K.
+
+renda_min(K, Y):- K is Y * 5000 + (4000 * Y).
+renda(adequada):- ganhos(X, estavel), dependentes(Y), renda_min(K, Y), X > K.
+renda(inadequada):- ganhos(X, estavel), dependentes(Y), renda_min(K, Y), X < K.
+
+investimentos(poupanca):- conta_poupanca(inadequada).
+investimentos(acoes):- conta_poupanca(adequada), renda(adequada).
+investimentos(combinacao):- conta_poupanca(adequada), renda(inadequada).
+investimos(tesouro direto):- conta_poupanca(inadequada),renda(adequada).
+investimos(fii):- conta_poupanca(adequada),renda(adequada).
+investimentos(imoveis):- conta_poupanca(adequada),renda(inadequada).
